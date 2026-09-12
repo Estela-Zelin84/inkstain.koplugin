f8ec8a147e086e324c358d08e597416201ac0365
Zelin <85815296+Estela-Zelin84@users.noreply.github.com>
2026-09-12 21:12:16 +0800

    Update fullname and version in _meta.lua

--- _meta.lua
+++ _meta.lua
@@ -1,7 +1,7 @@
 local _ = require("gettext")
 
 return {
-    fullname = _("墨痕壁纸"),
-    version = "3.9.3",
+    fullname = _("墨痕inkstain"),
+    version = "3.9.5",
     description = _([[根据 KOReader 阅读统计生成"墨痕账单"风格休眠壁纸。]]),
 }
