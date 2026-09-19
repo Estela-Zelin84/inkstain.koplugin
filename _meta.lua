5cf87438de497dac8645f05a2f9d9ef2883ce449
Zelin <85815296+Estela-Zelin84@users.noreply.github.com>
2026-09-19 09:46:42 +0800

    Update version to 3.9.7 in _meta.lua

--- _meta.lua
+++ _meta.lua
@@ -2,6 +2,6 @@ local _ = require("gettext")
 
 return {
     fullname = _("墨痕inkstain"),
-    version = "3.9.6",
+    version = "3.9.7",
     description = _([[根据 KOReader 阅读统计生成"墨痕账单"风格休眠壁纸。]]),
 }
